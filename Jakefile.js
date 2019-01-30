d382fa876dfd1b3a13ebd7d3e5bbbfeec291d7d1
Tommy Wilczk <tommywilczek@utexas.edu>
2019-01-29 20:58:11 -0600

    build checks node version

--- Jakefile.js
+++ Jakefile.js
@@ -1,9 +1,21 @@
 (function () {
     "use strict";
 
-    desc("Default Build")
-    task("default", function () {
+    var EXPECTED_NODE_VERSION = "v10.13.0"; //const in all caps, a convention
+
+    desc("Default Build");
+    task("default", ["version"], function () {
         console.log("\n\nBUILD OK");
+    });
+
+    desc("Check Node version");
+    task("version", function () {
+        console.log("Checking Node version: .");
+
+        let actualVersion = process.version;
+        if (actualVersion !== EXPECTED_NODE_VERSION) {
+            fail("Your Node version " + actualVersion + " does not match expected version: " + EXPECTED_NODE_VERSION)
+        }
     });
 
 }());
