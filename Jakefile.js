d55c26d5d772be53bc3d60586638f48af05d7135
Tommy Wilczk <tommywilczek@utexas.edu>
2019-01-31 12:26:40 -0600

    added test.js and now linting all files

--- Jakefile.js
+++ Jakefile.js
@@ -41,7 +41,7 @@
         process.stdout.write("Linting JavaScript: ");
 
         jshint.checkFiles({
-            files: "Jakefile.js",
+            files: ["Jakefile.js", "src/**/*.js"],
             options: {
                 bitwise: true,
                 eqeqeq: true,
