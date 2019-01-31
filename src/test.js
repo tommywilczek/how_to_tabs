f33179a1ad71c8dfe9e6fc3f49a621364350ee2a
Tommy Wilczk <tommywilczek@utexas.edu>
2019-01-31 12:36:43 -0600

    simple chai assertion set up

--- src/test.js
+++ src/test.js
@@ -1,4 +1,12 @@
-(function () {
+(function() {
     "use strict";
+
+    var assert = require("chai").assert;
+
+    assert.equal(add(3, 4), 7);
+
+    function add(a, b) {
+        return a + b;
+    }
 
 }());
