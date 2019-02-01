889f7be6fb678f1761065741fe20c6c66a293301
Tommy Wilczk <tommywilczek@utexas.edu>
2019-01-31 18:32:57 -0600

    option added for loose cross-browser testing

--- Jakefile.js
+++ Jakefile.js
@@ -69,7 +69,8 @@
                 "Chrome 71.0.3578 (Mac OS X 10.14.2)",
                 "Firefox 65.0.0 (Mac OS X 10.14.0)",
                 "Safari 12.0.2 (Mac OS X 10.14.2)"
-            ]
+            ],
+            strict: !process.env.loose //add 'loose=true' to run
         }, complete, fail);
     }, { async: true });
 
