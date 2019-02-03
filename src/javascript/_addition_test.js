be46a454297640aa25e68338dd628019088d5593
Tommy Wilczk <tommywilczek@utexas.edu>
2019-02-03 11:46:05 -0600

    installed chai for assertions

--- src/javascript/_addition_test.js
+++ src/javascript/_addition_test.js
@@ -2,20 +2,17 @@
     "use strict";
 
     var addition = require("./addition");
+    var assert = require("../vendor/chai-4.2.0").assert;
 
     describe("Addition", function() {
 
         it("adds positive numbers", function() {
-            assertEqual(addition.add(3, 4), 7);
+            assert.equal(addition.add(3, 4), 7);
         });
 
         it("uses IEEE 754 floating point", function() {
-            assertEqual(addition.add(0.1, 0.2), 0.30000000000000004);
+            assert.equal(addition.add(0.1, 0.2), 0.30000000000000004);
         });
-
-        function assertEqual(actual, expected) {
-            if (actual !== expected) throw new Error("expected " + expected + ", but was " + actual);
-        }
 
     });
 
