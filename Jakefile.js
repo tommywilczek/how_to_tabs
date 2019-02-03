3f35bacc12f30236ec014308ada631cbb7f17d4d
Tommy Wilczk <tommywilczek@utexas.edu>
2019-02-02 22:23:14 -0600

    separated content and code in src directory

--- Jakefile.js
+++ Jakefile.js
@@ -61,7 +61,7 @@
         process.stdout.write("Linting JavaScript: ");
 
         jshint.checkFiles({
-            files: ["Jakefile.js", "src/**/*.js"],
+            files: ["Jakefile.js", "src/javascript/**/*.js"],
             options: lintOptions(),
             globals: lintGlobals()
         }, complete, fail);
@@ -89,9 +89,9 @@
         console.log("Building distribution directory: .");
 
         shell.rm("-rf", DIST_DIR + "/*");
-        shell.cp("src/index.html", DIST_DIR);
+        shell.cp("src/content/*", DIST_DIR);
 
-        jake.exec("node node_modules/browserify/bin/cmd.js src/app.js -o " + DIST_DIR + "/bundle.js", {interactive: true}, complete);
+        jake.exec("node node_modules/browserify/bin/cmd.js src/javascript/app.js -o " + DIST_DIR + "/bundle.js", {interactive: true}, complete);
     }, { async: true });
 
     directory(DIST_DIR);
