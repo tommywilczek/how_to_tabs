ce406fd0c92187522bb5337a9fb47eb4c64cc48e
Tommy Wilczk <tommywilczek@utexas.edu>
2019-02-12 18:16:04 -0600

    use classlist rather than setting class attribute

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -2,13 +2,13 @@
     "use strict";
 
     exports.initialize = function initialize(element, className) {
-
-        var existingClasses = element.getAttribute("class");
-        if (existingClasses === null) {
-            element.setAttribute("class", className);
-        }
-        else {
-            element.setAttribute("class", existingClasses + " " + className);
-        }
+        element.classList.add(className);
+        // var existingClasses = element.getAttribute("class");
+        // if (existingClasses === null) {
+        //     element.setAttribute("class", className);
+        // }
+        // else {
+        //     element.setAttribute("class", existingClasses + " " + className);
+        // }
     };
 }());
